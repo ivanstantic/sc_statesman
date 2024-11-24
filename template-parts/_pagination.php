32b1a40c4aa4b10156458d0eb4f2fa988cef9a6c
Ivan Stantic <stantic.ivan@gmail.com>
2024-11-24 16:05:15 +0100

    Working on the single post template

--- template-parts/_pagination.php
+++ template-parts/_pagination.php
@@ -7,12 +7,12 @@ $pagination_args = [
     'mid_size'  => 2,
     'prev_text' => __( '« Previous', 'textdomain' ),
     'next_text' => __( 'Next »', 'textdomain' ),
-    'before_page_number' => '<span class="px-3 py-2 sm:mb-0 mb-2 bg-gray-200 dark:bg-[#1B2228] text-[#1B2228] dark:text-[#F2F3F4] rounded border border-gray-300 dark:border-gray-600">',
+    'before_page_number' => '<span class="px-3 py-2 sm:mb-0 mb-2 bg-gray-200 dark:bg-[#1B2228] rounded border border-gray-300 dark:border-gray-600">',
     'after_page_number'  => '</span>',
 ];
 ?>
 
-<nav class="pagination flex flex-wrap flex-col sm:flex-row items-center lg:justify-start justify-center mt-8 mb-8 space-y-4 sm:space-y-0">
+<nav class="pagination flex flex-wrap flex-col sm:flex-row items-center lg:justify-start justify-center mt-8 mb-8 space-y-4 sm:space-y-0 text-[#1B2228] dark:text-[#F2F3F4]">
     <span class="text-[#1B2228] dark:text-white md:mr-4 mr-0 md:mb-0 mb-4">
         Page <?php echo $pagination_args['current']; ?> of <?php echo $pagination_args['total']; ?>
     </span>
--- template-parts/single/_entry.php
+++ template-parts/single/_entry.php
@@ -1,51 +1,62 @@
 
-<!-- Article Hero-->
+<!-- Article Header -->
 <header class="w-full flex items-center justify-center  ">
-    <div class="lg:w-[80%] w-[90%]">
+    <div class="w-full p-4 md:p-0 max-w-[1344px]">
         <div class="flex justify-center items-center flex-col mt-[3rem]">
 
             <!-- Header Section -->
-            <div class="xl:w-[60%] lg:w-[80%]">
-                <p class="text-blue-600 text-[14px] font-bold font-roboto-serif">State</p>
-                <h1 class="lg:text-[3rem] md:text-[2.5rem] sm:text-[2.5rem] text-[2rem]  leading-[52px] font-bold mt-2 w-[77%]">
-                    South Carolina's Coastal Development
+            <div class="w-full max-w-[750px]">
+                <?php if (!empty(get_the_category())) : ?>
+                    <div class="text-blue-600 text-[14px] font-bold font-roboto-serif">
+                        <?php the_category(', '); ?>
+                    </div>
+                <?php endif; ?>
+                <h1 class="lg:text-[3rem] md:text-[2.5rem] sm:text-[2.5rem] text-[2rem] leading-[52px] font-bold mt-2 w-[77%]">
+                    <?php the_title(); ?>
                 </h1>
-                <p class="text-gray-600 mt-2 text-[20px] dark:text-white">
+                <!-- <p class="text-gray-600 mt-2 text-[20px] dark:text-white">
                     An In-depth Look at the Recent Developments Along South Carolina's Coastline
-                </p>
+                </p> -->
             </div>
 
             <!-- Image Section -->
-            <div class="mt-6">
-                <img
-                    src="assets/images/article-hero.png" 
+            <div class="mt-6 w-full max-w-[996px] aspect-[996/474] overflow-hidden">
+                <?php if (has_post_thumbnail()) : ?>
+                    <?php the_post_thumbnail('thumbnail_single', [ 'class' => 'w-full h-full object-cover rounded-lg shadow-lg' ]); ?>
+                <?php endif; ?>
+                <!-- <img
+                    src="<?php echo get_stylesheet_directory_uri(); ?>/_assets/public/images/article-hero.png" 
                     alt="South Carolina Coastline" 
                     class="rounded-lg shadow-lg"
-                />
+                /> -->
             </div>
             
             <!-- Author Info Section -->
-            <div class="flex items-center mt-4 xl:w-[60%] lg:w-[80%] w-[100%]">
-                <img 
-                    src="assets/images/pfp.png" 
-                    alt="Author Avatar" 
-                    class="w-10 h-10 rounded-full mr-4"
-                />
+            <div class="mt-4 w-full max-w-[750px] flex items-center">
+                <?php echo get_avatar(get_the_author_meta('ID'), 40, '', '', [
+                    'class' => 'w-10 h-10 rounded-full mr-4',
+                ]); ?>
                 <div>
-                    <p class="font-bold text-gray-800 font-[16px] dark:text-white">Alexis Monroe</p>
-                    <p class="text-[14px] text-gray-500">Senior Reporter</p>
+                    <p class="font-bold text-gray-800 dark:text-white">
+                        <a href="<?php echo scs_get_author_url(); ?>">
+                            <?php the_author(); ?>
+                        </a>
+                    </p>
+                    <!-- <p class="text-[14px] text-gray-500">Senior Reporter</p> -->
                 </div>
             </div>
         
             <!-- Date Section -->
-            <p class="text-[14px] text-gray-500 mt-2 xl:w-[60%] lg:w-[80%] w-[100%]">October 10, 2023</p>
+            <p class="mt-2 w-full max-w-[750px] text-[14px] text-gray-500">
+                <?php echo get_the_date('F j, Y'); ?>
+            </p>
         </div>
     </div>
 </header>
 
 <main class="w-full flex items-center justify-center bg-[#f2f3f4] dark:bg-[#1B2228] pt-10 pb-14">
-    <div class="md:w-[80%] w-[90%]">
-        <div class="xl:w-[60%] lg:w-[80%]  self-center mx-auto border-t-2 pt-10">
+    <div class="w-full p-4 md:p-0 max-w-[750px]">
+        <div class="mx-auto pt-10 self-center border-t-2">
             <!-- <hr class="border-none h-[1px] bg-gray-600 m-8 w-[100%]" /> -->
             <!-- Title Section -->
             <h1 class="text-[24px] font-bold font-roboto-serif">An In-depth Look at the Recent Developments Along South Carolina's Coastline</h1>
@@ -62,7 +73,7 @@
             <!-- Image Section -->
             <div class="mt-6">
                 <img
-                src="assets/images/article-1.png" 
+                src="<?php echo get_stylesheet_directory_uri(); ?>/_assets/public/images/article-1.png" 
                 alt="New commercial area" 
                 class="w-[100%]"
                 />
@@ -84,7 +95,7 @@
             <!-- Podcast Embed Section -->
             <div class="mt-8 rounded-lg flex items-start">
                 <img
-                src="assets/images/audio-placeholder.png" 
+                src="<?php echo get_stylesheet_directory_uri(); ?>/_assets/public/images/audio-placeholder.png" 
                 alt="Podcast Thumbnail" 
                 class="w-[100%]"
                 />
@@ -112,7 +123,7 @@
 <div class="w-full flex items-center justify-center bg-[#f2f3f4] dark:bg-[#1B2228]">
     <div class="xl:w-[70%] lg:w-[80%] w-[90%] grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 py-8">
         <div class="dark:bg-[#1B2228] overflow-hidden">
-            <img src="assets/images/grid/grid-9.png" alt="Card Image"
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/_assets/public/images/grid/grid-9.png" alt="Card Image"
                 class="w-full h-[220px] object-cover rounded" />
             <div class="py-4">
                 <h3
@@ -125,7 +136,7 @@
             </div>
         </div>
         <div class="dark:bg-[#1B2228] overflow-hidden">
-            <img src="assets/images/grid/grid-10.png" alt="Card Image"
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/_assets/public/images/grid/grid-10.png" alt="Card Image"
                 class="w-full h-[220px] object-cover rounded" />
             <div class="py-4">
                 <h3
@@ -138,7 +149,7 @@
             </div>
         </div>
         <div class="dark:bg-[#1B2228] overflow-hidden">
-            <img src="assets/images/grid/grid-11.png" alt="Card Image"
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/_assets/public/images/grid/grid-11.png" alt="Card Image"
                 class="w-full h-[220px] object-cover rounded" />
             <div class="py-4">
                 <h3
@@ -156,7 +167,7 @@
 
 <hr class="border-none h-[1px] bg-[#cbd5e1]" />
 <div class="w-full flex items-center justify-center bg-[#f2f3f4] dark:bg-[#1B2228]">
-    <div class="w-[90%] flex justify-between items-center pt-[24px] pb-[0px]">
+    <div class="w-[90%] max-w-[1344px] flex justify-between items-center pt-[24px] pb-[0px]">
         <div class="text-[32px] font-bold">
             <p class="dark:text-white text-[#1B2228]">Latest News</p>
             <hr class="w-[20%] border-none h-[6px] bg-[#2D5599]" />
@@ -167,9 +178,9 @@
     </div>
 </div>
 <div class="w-full flex items-center justify-center bg-[#f2f3f4] dark:bg-[#1B2228]">
-    <div class="w-[90%] grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 py-8">
+    <div class="w-[90%] max-w-[1344px] grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 py-8">
         <div class="dark:bg-[#1B2228] overflow-hidden">
-            <img src="assets/images/grid/grid-9.png" alt="Card Image"
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/_assets/public/images/grid/grid-9.png" alt="Card Image"
                 class="w-full h-[220px] object-cover rounded" />
             <div class="py-4">
                 <h3
@@ -182,7 +193,7 @@
             </div>
         </div>
         <div class="dark:bg-[#1B2228] overflow-hidden">
-            <img src="assets/images/grid/grid-10.png" alt="Card Image"
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/_assets/public/images/grid/grid-10.png" alt="Card Image"
                 class="w-full h-[220px] object-cover rounded" />
             <div class="py-4">
                 <h3
@@ -195,7 +206,7 @@
             </div>
         </div>
         <div class="dark:bg-[#1B2228] overflow-hidden">
-            <img src="assets/images/grid/grid-11.png" alt="Card Image"
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/_assets/public/images/grid/grid-11.png" alt="Card Image"
                 class="w-full h-[220px] object-cover rounded" />
             <div class="py-4">
                 <h3
@@ -208,7 +219,7 @@
             </div>
         </div>
         <div class="dark:bg-[#1B2228] overflow-hidden">
-            <img src="assets/images/grid/grid-4.png" alt="Card Image"
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/_assets/public/images/grid/grid-4.png" alt="Card Image"
                 class="w-full h-[220px] object-cover rounded" />
             <div class="py-4">
                 <h3
@@ -221,7 +232,7 @@
             </div>
         </div>
         <div class="dark:bg-[#1B2228] overflow-hidden">
-            <img src="assets/images/grid/grid-5.png" alt="Card Image"
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/_assets/public/images/grid/grid-5.png" alt="Card Image"
                 class="w-full h-[220px] object-cover rounded" />
             <div class="py-4">
                 <h3
@@ -234,7 +245,7 @@
             </div>
         </div>
         <div class="dark:bg-[#1B2228] overflow-hidden">
-            <img src="assets/images/grid/grid-6.png" alt="Card Image"
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/_assets/public/images/grid/grid-6.png" alt="Card Image"
                 class="w-full h-[220px] object-cover rounded" />
             <div class="py-4">
                 <h3
@@ -247,7 +258,7 @@
             </div>
         </div>
         <div class="dark:bg-[#1B2228] overflow-hidden">
-            <img src="assets/images/grid/grid-7.png" alt="Card Image"
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/_assets/public/images/grid/grid-7.png" alt="Card Image"
                 class="w-full h-[220px] object-cover rounded" />
             <div class="py-4">
                 <h3
@@ -260,7 +271,7 @@
             </div>
         </div>
         <div class="dark:bg-[#1B2228] overflow-hidden">
-            <img src="assets/images/grid/grid-8.png" alt="Card Image"
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/_assets/public/images/grid/grid-8.png" alt="Card Image"
                 class="w-full h-[220px] object-cover rounded" />
             <div class="py-4">
                 <h3
