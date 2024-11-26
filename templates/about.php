a48983afd3b72a5e3229cda4fa3ad5b802b01307
Ivan Stantic <stantic.ivan@gmail.com>
2024-11-26 16:50:46 +0100

    Update the about us page

--- templates/about.php
+++ templates/about.php
@@ -7,14 +7,14 @@
     <!-- Hero Section -->
     <div class="w-full flex items-center justify-center bg-[#f2f3f4] dark:bg-[#1B2228] md:pt-10 md:pb-10 sm:pt-8 sm:pb-8 dark:pb-2">
         <div class="w-[90%] max-w-[1344px]">
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6 h-[100%]">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-6 h-[100%]">
 
                 <!-- About Us Text -->
                 <div class="md:col-span-2 flex items-start justify-center flex-col">
                     <h2 class="lg:text-[3rem] md:text-[2.5rem] sm:text-[2.5rem] text-[2rem] font-roboto-serif font-bold text-[#1B2228] sm:mb-4 mb-2 dark:text-[#F2F3F4]">About Us</h2>
                     <div class="text-gray-600 mb-4 dark:text-white lg:text-[1.25rem] md:text-[1rem]">
                         <p class="mb-4">The Statesman is a part of a long tradition of South Carolina independent news from the South Carolina State Gazette and Daily Advertiser in 1784 to today. Like South Carolina, we have a conservative lean, but we recognize no side of the aisle has a monopoly on insight or ideas. Our goal is to provide news and insight that leads to better representation and promotes good governance for the people of South Carolina.</p>
-                        <p class="mb-4">The Statesman is led by [insert name: Matthew Berry] and supported by a network of local writers from Columbia, Charleston, Greenville, Rock Hill and others. We want to capture what’s going on in the Statehouse and see first-hand how it’s playing out on Main Street. We see our stories and coverage as part of the dialog between South Carolinians and those who represent them. It’s our privilege to surface the cares and concerns of South Carolinians and to shine a light on the good works of the people making a real difference for us all.</p>
+                        <p class="mb-4">The Statesman is led by Matthew Berry and supported by a network of local writers from Columbia, Charleston, Greenville, Rock Hill and others. We want to capture what’s going on in the Statehouse and see first-hand how it’s playing out on Main Street. We see our stories and coverage as part of the dialog between South Carolinians and those who represent them. It’s our privilege to surface the cares and concerns of South Carolinians and to shine a light on the good works of the people making a real difference for us all.</p>
                         <p class="mb-4">This is an exciting time for our State with our leaders and businesses at the forefront of so many issues of national significance—energy, immigration and innovation. South Carolina has tremendous heritage and history, but an even brighter future.</p>
                     </div>
                 </div>
