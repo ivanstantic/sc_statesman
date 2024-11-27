1b779feb6895654b74a97b8e4ada76d7a49e6588
Ivan Stantic <stantic.ivan@gmail.com>
2024-11-27 08:59:57 +0100

    Refactor the main menu into two menus

--- _core/menus.php
+++ _core/menus.php
@@ -4,7 +4,8 @@
 function scs_menus()
 {
     register_nav_menus([
-        'scs_main_menu' => 'Main Menu',
+        'scs_main_menu'   => 'Main Menu',
+        'scs_second_menu' => 'Second Menu',
     ]);
 }
 add_action('init', 'scs_menus');
--- header.php
+++ header.php
@@ -1,52 +1,3 @@
-<?php
-
-$locations = get_nav_menu_locations();
-$menu_location = 'scs_main_menu';
-$menu = array();
-if ( isset( $locations[ $menu_location ] ) ) :
-    global $wp;
-    $permalink = home_url( $wp->request );
-    $menu_id = $locations[ $menu_location ];
-    $menu_items = wp_get_nav_menu_items( $menu_id );
-
-    foreach ( $menu_items as $menu_item ) :
-        $id = $menu_item->ID;
-        $title = $menu_item->title;
-        $link = $menu_item->url;
-        $menu_item_parent = $menu_item->menu_item_parent;
-
-        // if menu item has no parent, means this is the top-menu.
-        if ( ! $menu_item_parent ) {
-            $menu[ $id ]['active'] = false;
-            $menu[ $id ]['children'] = array();
-            $menu[ $id ]['id'] = $id;
-            $menu[ $id ]['title'] = $title;
-            $menu[ $id ]['link'] = $link;
-
-            // add active field if current link and open url is same.
-            if ( trailingslashit( $permalink ) === trailingslashit( $link ) ) {
-                $menu[ $id ]['active'] = true;
-            }
-        } else {
-            // if parent menu is set, means this is 2nd level menu
-            if ( isset( $menu[ $menu_item_parent ] ) ) {
-                $menu[ $menu_item_parent ]['children'][ $id ]['active'] = false;
-                $menu[ $menu_item_parent ]['children'][ $id ]['id'] = $id;
-                $menu[ $menu_item_parent ]['children'][ $id ]['title'] = $title;
-                $menu[ $menu_item_parent ]['children'][ $id ]['link'] = $link;
-
-                // add active field to current menu item and its parent menu item if current link and open url is same.
-                if ( trailingslashit( $permalink ) === trailingslashit( $link ) ) {
-                    $menu[ $menu_item_parent ]['active'] = true;
-                    $menu[ $menu_item_parent ]['children'][ $id ]['active'] = true;
-                }
-            }
-        }
-    endforeach;
-endif;
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -74,24 +25,28 @@ endif;
             <!-- Navigation Links -->
             <!-- <nav class="hidden md:flex items-center space-x-4 lg:space-x-12 text-sm mt-4 md:mt-0">
                 <?php
-                    if ( isset( $locations[ $menu_location ] ) ) :
-                        if ( ! empty( $menu ) ) :
-                            foreach ( $menu as $item ) :
-                                if ( $item['active'] ) :
-                                    echo '<span class="uppercase font-bold">' . esc_html( $item['title'] ) . '</span>';
-                                else :
-                                    echo '<a href="' . esc_url( $item['link'] ) . '" class="hover:text-gray-300 uppercase">';
-                                    echo  esc_html( $item['title'] );
-                                    echo '</a>';
-                                endif;
-                            endforeach;
-                        else :
-                            echo 'No menu items found for the assigned menu.';
-                        endif;
-                    else :
-                        echo 'No menu assigned to this theme location.';
-                    endif;
+                    $menu_location = 'scs_main_menu';
+                    $locations = get_nav_menu_locations();
                 ?>
+                <?php if ( isset($locations[$menu_location] ) ) : ?>
+                    <?php
+                        $menu = wp_get_nav_menu_object( $locations[$menu_location] );
+                        $menu_items = wp_get_nav_menu_items( $menu->term_id );
+                    ?>
+                    <?php if ( $menu_items ) : ?>
+                        <?php foreach( $menu_items as $item ) : ?>
+                            <?php if( in_array('current-menu-item', $item->classes) ) : ?>
+                                <span class="uppercase font-bold">
+                                    <?php echo esc_html( $item->title ); ?>
+                                </span>
+                            <?php else: ?>
+                                <a href="<?php echo esc_url( $item->url ); ?>" class="hover:text-gray-300 uppercase">
+                                    <?php echo esc_html( $item->title ); ?>
+                                </a>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                <?php endif; ?>
             </nav> -->
 
             <!-- Actions: Subscribe, Search, Theme Toggle -->
@@ -113,8 +68,8 @@ endif;
                 </div>
             </div>
 
-            <!-- <div class="md:hidden flex items-center"> -->
-                <!-- <button id="mobile-menu-toggle" class="text-white focus:outline-none"> -->
+            <!-- <div class="md:hidden flex items-center">
+                <button id="mobile-menu-toggle" class="text-white focus:outline-none"> -->
                     <!-- Hamburger Icon -->
                     <!-- <svg
                         id="hamburger-icon"
@@ -146,31 +101,35 @@ endif;
                             stroke-width="2"
                             d="M6 18L18 6M6 6l12 12"
                         ></path>
-                    </svg> -->
-                <!-- </button> -->
-            <!-- </div> -->
+                    </svg>
+                </button>
+            </div> -->
 
             <!-- Mobile Navigation -->
             <!-- <nav id="mobile-menu" class="hidden w-full mt-4 md:hidden flex flex-col items-center space-y-2 text-sm bg-[#1B3664] px-4 py-2 rounded-md">
                 <?php
-                    if ( isset( $locations[ $menu_location ] ) ) :
-                        if ( ! empty( $menu ) ) :
-                            foreach ( $menu as $item ) :
-                                if ( $item['active'] ) :
-                                    echo '<span class="uppercase font-bold">' . esc_html( $item['title'] ) . '</span>';
-                                else :
-                                    echo '<a href="' . esc_url( $item['link'] ) . '" class="hover:text-gray-300 uppercase">';
-                                    echo  esc_html( $item['title'] );
-                                    echo '</a>';
-                                endif;
-                            endforeach;
-                        else :
-                            echo 'No menu items found for the assigned menu.';
-                        endif;
-                    else :
-                        echo 'No menu assigned to this theme location.';
-                    endif;
+                    $menu_location = 'scs_main_menu';
+                    $locations = get_nav_menu_locations();
                 ?>
+                <?php if ( isset($locations[$menu_location] ) ) : ?>
+                    <?php
+                        $menu = wp_get_nav_menu_object( $locations[$menu_location] );
+                        $menu_items = wp_get_nav_menu_items( $menu->term_id );
+                    ?>
+                    <?php if ( $menu_items ) : ?>
+                        <?php foreach( $menu_items as $item ) : ?>
+                            <?php if( in_array('current-menu-item', $item->classes) ) : ?>
+                                <span class="uppercase font-bold">
+                                    <?php echo esc_html( $item->title ); ?>
+                                </span>
+                            <?php else: ?>
+                                <a href="<?php echo esc_url( $item->url ); ?>" class="hover:text-gray-300 uppercase">
+                                    <?php echo esc_html( $item->title ); ?>
+                                </a>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                <?php endif; ?>
             </nav> -->
         </div>
     </header>
@@ -179,20 +138,30 @@ endif;
     <div class="w-full py-2 flex items-center justify-center bg-white dark:bg-black">
         <div class="w-[90%] max-w-[1344px]">
             <!-- Navigation Bar -->
-            <?php if ( ! empty( $menu ) ) : ?>
-                <nav class="py-1.5 flex items-center space-x-6 text-sm text-[#1B3664] dark:text-[#7898FF] font-medium ">
-                    <?php
-                        foreach ( $menu as $item ) :
-                            if ( $item['active'] && ! empty( $item['children'] ) ) :
-                                foreach ( $item[ 'children' ] as $child ) :
-                                    echo '<a href="' . esc_url( $child['link'] ) . '" hover:text-gray-300 whitespace-nowrap uppercase>';
-                                    echo esc_html( $child['title'] );
-                                    echo '</a>';
-                                endforeach;
-                            endif;
-                        endforeach;
-                    ?>
-                </nav>
+            <?php
+                $menu_location = 'scs_second_menu';
+                $locations = get_nav_menu_locations();
+            ?>
+            <?php if ( isset($locations[$menu_location] ) ) : ?>
+                <?php
+                    $menu = wp_get_nav_menu_object( $locations[$menu_location] );
+                    $menu_items = wp_get_nav_menu_items( $menu->term_id );
+                ?>
+                <?php if ( $menu_items ) : ?>
+                    <nav class="py-1.5 flex items-center space-x-6 text-sm text-[#1B3664] dark:text-[#7898FF] font-medium ">
+                        <?php foreach( $menu_items as $item ) : ?>
+                            <?php if( in_array('current-menu-item', $item->classes) ) : ?>
+                                <span class="whitespace-nowrap uppercase font-bold">
+                                    <?php echo esc_html( $item->title ); ?>
+                                </span>
+                            <?php else: ?>
+                                <a href="<?php echo esc_url( $item->url ); ?>" class="hover:text-gray-300 whitespace-nowrap uppercase">
+                                    <?php echo esc_html( $item->title ); ?>
+                                </a>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                    </nav>
+                <?php endif; ?>
             <?php endif; ?>
         </div>
     </div>
