3f0a28834fedf9866c908e909af8fe3e8537e391
Ivan Stantic <stantic.ivan@gmail.com>
2024-11-28 13:20:09 +0100

    Add more vertical spacing

--- _core/hooks/filters.php
+++ _core/hooks/filters.php
@@ -4,3 +4,10 @@ function hc_excerpt_length($length) {
     return 100; // Set the number of words for the excerpt
 }
 add_filter('excerpt_length', 'hc_excerpt_length', 999);
+
+function replace_br_with_div($content) {
+    // Use regular expression to find <br> tags and replace them with <div class="spacer"></div>
+    $content = preg_replace('/<br\s*\/?>/i', '<div class="spacer"></div>', $content);
+    return $content;
+}
+add_filter('the_content', 'replace_br_with_div');
