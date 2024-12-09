b77266466570f55b93048521a0ad88c9d4f2e862
Ivan Stantic <stantic.ivan@gmail.com>
2024-12-09 13:50:28 +0100

    Try a fix

--- _core/blocks/prose-wide/prose-wide.php
+++ _core/blocks/prose-wide/prose-wide.php
@@ -3,7 +3,7 @@
     $block_class = isset($block['className']) ? $block['className'] : '';
 ?>
 
-<section id="<?php echo esc_attr($block_id); ?>" class="<?php echo esc_attr($block_class); ?>">
+<div id="<?php echo esc_attr($block_id); ?>" class="<?php echo esc_attr($block_class); ?>">
     <div class="max-w-full lg:max-w-none w-[996px] relative left-1/2 -translate-x-1/2">
         <InnerBlocks />
     </div>
