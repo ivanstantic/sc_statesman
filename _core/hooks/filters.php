210f594ce32de6ef6815db8072f455ec2b12baa7
Ivan Stantic <stantic.ivan@gmail.com>
2024-12-09 13:59:21 +0100

    Fix fixed colors

--- _core/hooks/filters.php
+++ _core/hooks/filters.php
@@ -6,6 +6,12 @@ function hc_excerpt_length($length) {
 add_filter('excerpt_length', 'hc_excerpt_length', 999);
 
 
+function replace_br_with_div($content) {
+    // Use regular expression to find <br> tags and replace them with <span class="spacer"></span>
+    $content = preg_replace('/<br\s*\/?>/i', '<span class="spacer"></span>', $content);
+    return $content;
+}
+add_filter('the_content', 'replace_br_with_div');
 
 
 /**
